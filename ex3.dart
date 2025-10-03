16278bc8997b0be272eecfb81a9dc6a1c5b19f65
Bruno Rodovicks <66980466+rodovicks@users.noreply.github.com>
2025-10-02 23:26:26 -0300

    Update ex3.dart

--- ex3.dart
+++ ex3.dart
@@ -1,7 +1,30 @@
 void main() {
-  final now = DateTime.now();
+  DateTime hoje = DateTime.now();
+  int ano = hoje.year;
+  int mes = hoje.month;
 
-  final firstDayMonth = DateTime.utc(now.year, now.month, 1);
+  DateTime primeiroDiaMes = DateTime(ano, mes, 1);
 
   print('| D | S | T | Q | Q | S | S |');
+
+  String linha = '';
+  for (int i = 0; i < primeiroDiaMes.weekday % 7; i++) {
+    linha += '    ';
+  }
+
+  for (int dia = 1; dia <= hoje.day; dia++) {
+    DateTime data = DateTime(ano, mes, dia);
+
+    String diaFormatado = dia.toString().padLeft(2, '0');
+    linha += ' $diaFormatado ';
+
+    if (data.weekday % 7 == 0) {
+      print(linha);
+      linha = '';
+    }
+  }
+
+  if (linha.isNotEmpty) {
+    print(linha);
+  }
 }
